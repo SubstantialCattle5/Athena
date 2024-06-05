5f9814f4b0e11e2f0e4a33900fc923b6ef0c727c
nilay nath sharan <nilaynathsharan16@gmail.com>
2024-06-06 00:13:35 +0530

    feat:google auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, UserModule],
 })
 export class AppModule {}
