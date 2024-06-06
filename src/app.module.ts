d805e4e95ab7e4460f56d4aabd39390bc18acfcf
nilay nath sharan <nilaynathsharan16@gmail.com>
2024-06-06 15:07:02 +0530

    add: survey

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BlogModule } from './blog/blog.module';
+import { SurveyModule } from './survey/survey.module';
 
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule,BlogModule],
+  imports: [PrismaModule, AuthModule, UserModule,BlogModule, SurveyModule],
 })
 export class AppModule {}
