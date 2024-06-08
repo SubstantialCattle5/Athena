ac81c11a16503f5e115d65656ba1758e77139791
nilay nath sharan <nilaynathsharan16@gmail.com>
2024-06-08 22:35:06 +0530

    refactor: user code

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,33 +1,41 @@
 import { IsString, IsInt, IsEmail, IsDateString, IsEnum, IsOptional } from 'class-validator';
 import { Gender, Position } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'The name of the user.' })
   @IsString()
   name: string;
 
+  @ApiProperty({ description: 'The age of the user.' })
   @IsInt()
   age: number;
 
+  @ApiProperty({ description: 'The birthday of the user.' })
   @IsDateString()
   birthday: Date;
 
+  @ApiProperty({ description: 'The location of the user.' })
   @IsString()
   location: string;
 
+  @ApiProperty({ description: 'The gender of the user.', enum: Gender })
   @IsEnum(Gender)
   gender: Gender;
 
+  @ApiProperty({ description: 'The email of the user.' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ description: 'The contact number of the user.' })
   @IsString()
   contact: string;
 
+  @ApiProperty({ description: 'The picture URL of the user.' })
   @IsString()
   picture: string;
 
+  @ApiProperty({ description: 'The position of the user.', enum: Position })
   @IsEnum(Position)
   position: Position;
 }
-
-
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,40 +1,49 @@
-// src/user/dto/update-user.dto.ts
 import { IsString, IsInt, IsEmail, IsDateString, IsEnum, IsOptional } from 'class-validator';
 import { Gender, Position } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
+  @ApiProperty({ description: 'The name of the user.', required: false })
   @IsString()
   @IsOptional()
   name?: string;
 
+  @ApiProperty({ description: 'The age of the user.', required: false })
   @IsInt()
   @IsOptional()
   age?: number;
 
+  @ApiProperty({ description: 'The birthday of the user.', required: false })
   @IsDateString()
   @IsOptional()
   birthday?: Date;
 
+  @ApiProperty({ description: 'The location of the user.', required: false })
   @IsString()
   @IsOptional()
   location?: string;
 
+  @ApiProperty({ description: 'The gender of the user.', enum: Gender, required: false })
   @IsEnum(Gender)
   @IsOptional()
   gender?: Gender;
 
+  @ApiProperty({ description: 'The email of the user.', required: false })
   @IsEmail()
   @IsOptional()
   email?: string;
 
+  @ApiProperty({ description: 'The contact number of the user.', required: false })
   @IsString()
   @IsOptional()
   contact?: string;
 
+  @ApiProperty({ description: 'The picture URL of the user.', required: false })
   @IsString()
   @IsOptional()
   picture?: string;
 
+  @ApiProperty({ description: 'The position of the user.', enum: Position, required: false })
   @IsEnum(Position)
   @IsOptional()
   position?: Position;
